f7408680ed452460e25025d3d78c9ea3ce4b86e2
DoVanNhat <dovannhatdn94@gmail.com>
2023-04-04 09:56:41 +0700

    do: thêm chú thích

--- Service/Service/Hubs/ChatHub.cs
+++ Service/Service/Hubs/ChatHub.cs
@@ -19,6 +19,7 @@ namespace Service.Hubs
             _connections = connections;
         }
 
+        // khi người dùng đóng conection của họ hoặc họ tắt app, chức năng này đc khởi động và thông báo đến tất cả người dùng trong group
         public override Task OnDisconnectedAsync(Exception exception)
         {
             if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
@@ -31,12 +32,15 @@ namespace Service.Hubs
             return base.OnDisconnectedAsync(exception);
         }
 
+        // đây là hàm bắt sk join của người dùng 
+        
         public async Task JoinRoom(UserConnection userConnection)
         {
+            // Tạo môt group và add người dùng vào bằng connectionId và room (do người dùng truyền lên)
             await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
 
             _connections[Context.ConnectionId] = userConnection;
-
+            // phẩn hồi cho các người dùng có trong group (room : do người dùng truyền lên )
             await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has joined {userConnection.Room}");
 
             await SendUsersConnected(userConnection.Room);
@@ -44,14 +48,18 @@ namespace Service.Hubs
 
         public async Task SendMessage(string message)
         {
+            // kiểm tra id conect của người dùng có tồn tại trong group đok không !
             if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
             {
                 await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", userConnection.User, message);
             }
         }
 
+        // phuong thức này để duyệt ds người dùng và gữi phản hồi cho tất cả họ
+        // Cho phép chúng ta biết ai còn lại trong room
         public Task SendUsersConnected(string room)
         {
+
             var users = _connections.Values
                 .Where(c => c.Room == room)
                 .Select(c => c.User);
--- mychat/src/App.js
+++ mychat/src/App.js
@@ -8,13 +8,17 @@ const App = () => {
   const [messages, setMessages] = useState([]);
   const [users, setUsers] = useState([]);
 
+  //Confiu khi có user joinRoom
   const joinRoom = async (user, room) => {
     try {
+      //tao một connect map với hub trên sv
       const connection = new HubConnectionBuilder()
         .withUrl("https://localhost:7232/chat")
         .configureLogging(LogLevel.Information)
         .build();
 
+      //Thiết lập các sự kiện lắng nghe nghe ReceiveMessage (nhan mess từ hub),UsersInRoom (nhận ds từ hub) ,
+      // onclose là sk khi đóng connect bởi phương thức closeConnection
       connection.on("ReceiveMessage", (user, message) => {
         setMessages(messages => [...messages, { user, message }]);
       });
@@ -28,15 +32,17 @@ const App = () => {
         setMessages([]);
         setUsers([]);
       });
-
+      //Start
       await connection.start();
+      //Call sự kiện JoinRoom 
       await connection.invoke("JoinRoom", { user, room });
+      // Lưu connect vào state
       setConnection(connection);
     } catch (e) {
       console.log(e);
     }
   }
-
+  // tạo phương thức thông báo có tin nhắn mới
   const sendMessage = async (message) => {
     try {
       await connection.invoke("SendMessage", message);
@@ -45,6 +51,7 @@ const App = () => {
     }
   }
 
+// tạo phương thức dóng connect hiện tại
   const closeConnection = async () => {
     try {
       await connection.stop();
@@ -57,6 +64,7 @@ const App = () => {
     <h2>MyChat</h2>
     <hr className='line mx-auto mb-3' />
     {!connection
+    //Khi có conect nó sẽ chuyển sang
       ? <Lobby joinRoom={joinRoom} />
       : <Chat sendMessage={sendMessage} messages={messages} users={users} closeConnection={closeConnection} />}
   </div>
